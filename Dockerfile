525526e98e0334757eb40ce3a43ecf5c37eef88b
overnew <ehdls1303@gmail.com>
2023-08-20 00:34:52 +0900

    feat: slack bot & workflow

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10
+
+WORKDIR /code
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY slackbot/ .
+
+CMD ["python", "./test.py"]
---
+++ slackbot/test.py
@@ -0,0 +1,50 @@
+import config
+import re
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from elasticConnect import getQueryToCloud
+import json
+
+bot_token = os.environ['bot_token']
+app = App(token=os.environ['app_token'])
+
+
+@app.event("app_mention")  # 앱을 언급했을 때
+def who_am_i(event, client, message, say):
+  print('event:', event)
+  print('client:', client)
+  print('message:', message)
+  say(f'저는 당신의 길을 넓혀주는 봇입니다. 안녕하세요! <@{event["user"]}>')
+
+@app.message(re.compile("lunch"))
+def regex(event, client, message, say):
+    say('eat burger')
+
+
+@app.message("최신 공고")
+def queryStart(event, client, message, say):
+    say(getQueryToCloud())
+    
+
+@app.event("workflow_published")
+def workflow(event, client, message, say):
+    print("됐드")
+    say("let's go")
+
+
+@app.message(re.compile("질의"))
+def regex2(event, client, message, say):
+#   print('event:', event)
+#   print('client:', client)
+#   print('message:', message)
+#   print(type(event))
+    print('message:', event['text'])
+    print(event['blocks'][0]['elements'])
+    say('찾았다 임마')
+  
+
+if __name__ == '__main__':
+    handler = SocketModeHandler(app_token=config.app_token,app=app)
+    handler.start()
+    
