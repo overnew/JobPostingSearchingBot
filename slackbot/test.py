e16e637723bad8651494a8715c9aba88ea4d3771
overnew <ehdls1303@gmail.com>
2023-08-20 15:10:39 +0900

    change env var name

--- slackbot/test.py
+++ slackbot/test.py
@@ -3,11 +3,11 @@ import re
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from elasticConnect import getQueryToCloud
+#from elasticConnect import getQueryToCloud
 import json
 
-bot_token = os.environ['bot_token']
-app = App(token=os.environ['app_token'])
+bot_token = os.environ['BOT_TOKEN']
+app = App(token=os.environ['APP_TOKEN'])
 
 
 @app.event("app_mention")  # 앱을 언급했을 때
@@ -22,10 +22,10 @@ def regex(event, client, message, say):
     say('eat burger')
 
 
-@app.message("최신 공고")
-def queryStart(event, client, message, say):
-    say(getQueryToCloud())
-    
+# @app.message("최신 공고")
+# def queryStart(event, client, message, say):
+#     say(getQueryToCloud())
+#
 
 @app.event("workflow_published")
 def workflow(event, client, message, say):
@@ -45,7 +45,6 @@ def regex2(event, client, message, say):
   
 
 if __name__ == '__main__':
-    handler = SocketModeHandler(app_token=os.environ['app_token'],app=app)
-    print("gogo")
+    handler = SocketModeHandler(app_token=os.environ['APP_TOKEN'],app=app)
     handler.start()
     
